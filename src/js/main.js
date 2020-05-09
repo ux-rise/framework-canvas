5bc0d155b8cf68cc8438fdd248b36d062edcd5ae
Hernando H <hernando.shaolin@gmail.com>
2020-05-09 17:19:32 -0500

    ajustes en la interfaz

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+//Cambiar la funcion de scoreGame por esta que esta aqui.
+
+function scoreGame(valor) {
+  puntaje = document.getElementById("cookies");
+  puntaje.innerHTML = score;
+  score += 10;
+}
